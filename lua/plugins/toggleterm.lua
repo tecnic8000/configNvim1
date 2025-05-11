df406e61bbe34a86a210de5999200d6ac02b84a1
harriL490 <tranhoangtd@gmail.com>
2025-05-11 21:55:39 +0700

    close1

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,5 +2,11 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {--[[ things you want to change go here]]
+    --  direction = 'float',
+    float_opts = {
+      width = 80,
+      height = 20,
+      winblend = 3,
+    },
   },
 }
